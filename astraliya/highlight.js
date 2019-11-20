245a76a562e72d7e340cec088c43ad9a35bae316
Mitali Vasisht <vamdev37@gmail.com>
2019-11-20 13:54:53 -0600

    just for review

---
+++ astraliya/highlight.js
@@ -0,0 +1,13 @@
+const initializeDetailButtonEvents = () => {
+    // CAN'T TOUCH THIS - START
+    const allCloseButtons = document.querySelectorAll(".button--close")
+
+    for (const btn of allCloseButtons) {
+        btn.addEventListener(
+            "click",
+            theEvent => {
+                const dialogElement = theEvent.target.parentNode
+                dialogElement.close()
+            }
+        )
+    }

c17f271e5c5a6c507998d34b1dfc06143448ac83
jansenv <jansenv44@gmail.com>
2019-12-03 11:37:27 -0600

    added cities list component & fixed highlights

---
+++ scripts/england/cities/citiesDataProvider.js
@@ -0,0 +1,13 @@
+const citiesCollection = [
+    {
+        city: "London",
+    },
+    {   city: "Oxford",
+    },
+    {   city: "Liverpool",
+    }
+]
+
+export const useCities = () => {
+    return citiesCollection;
+}
---
+++ scripts/england/main.js
@@ -0,0 +1,5 @@
+import citiesListComponent from "./cities/citiesList.js";
+import highlightBackgroundColor from '/highlight.js';
+
+citiesListComponent();
+highlightBackgroundColor();
